ba7437dab59c7b99daf67b47f044b81ed8d6314a
wietecha-mrzyglod <wietecha.katarzyna@gmail.com>
2018-08-04 23:32:26 +0200

    Created form and connected with vue instance data

--- js/app.js
+++ js/app.js
@@ -1,54 +1,71 @@
-var counter = Vue.component("counter",{
-  template: `
-    <div>
-      <div>Count: {{ this.count }}</div>
-      <button @click="increment">Increment by {{ this.addNumber }}</button>
-    </div>
-  `,
-  data(){
-    return {
-      count: 0
-    }
-  },
-  props: {
-    addNumber: {
-      type: Number,
-      default: 1
-    }
-  },
-  methods: {
-    increment(){
-      this.count += this.addNumber
-    }
-  }
-})
+var notes = {
 
-var changeText = Vue.component("change-text",{
-  template: `
-    <div>
-      <p>Change the text:</p>
-      <input v-model="myText.text" type="text" placeholder="Your text" />
-      <div>Uppercased: {{ upperText }}</div>
-    </div>
-  `,
-  data(){
-    return {
-      myText: {
-        text: ""
-      }
-    }
-  },
-  computed: {
-    upperText(){
-      return this.myText.text.toUpperCase()
-    }
-  }
-})
+}
+
 
 var app = new Vue ({
   el: "#app",
-  components: {
-    counter,
-    changeText
+  data: {
+    title: "Notemaster",
+    notes: notes,
+    note: {
+      title: "",
+      text: ""
+    }
   }
 })
+
+// var counter = Vue.component("counter",{
+//   template: `
+//     <div>
+//       <div>Count: {{ this.count }}</div>
+//       <button @click="increment">Increment by {{ this.addNumber }}</button>
+//     </div>
+//   `,
+//   data(){
+//     return {
+//       count: 0
+//     }
+//   },
+//   props: {
+//     addNumber: {
+//       type: Number,
+//       default: 1
+//     }
+//   },
+//   methods: {
+//     increment(){
+//       this.count += this.addNumber
+//     }
+//   }
+// })
+//
+// var changeText = Vue.component("change-text",{
+//   template: `
+//     <div>
+//       <p>Change the text:</p>
+//       <input v-model="myText.text" type="text" placeholder="Your text" />
+//       <div>Uppercased: {{ upperText }}</div>
+//     </div>
+//   `,
+//   data(){
+//     return {
+//       myText: {
+//         text: ""
+//       }
+//     }
+//   },
+//   computed: {
+//     upperText(){
+//       return this.myText.text.toUpperCase()
+//     }
+//   }
+// })
+//
+// var app = new Vue ({
+//   el: "#app",
+//   components: {
+//     counter,
+//     changeText
+//   }
+// })
