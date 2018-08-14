0a1ed05c60975133b761d23b67a3019fabba05ef
Katarzyna Wietecha-Mrzyglod <wietecha.katarzyna@gmail.com>
2018-08-14 10:14:34 +0200

    Added: Importance option (and sorting)

--- js/app.js
+++ js/app.js
@@ -3,31 +3,36 @@ var notes = [
     title: "Buy a gift for sister",
     text: "Black handbag with a zipper and very big chocolate with nuts and raisins.",
     date: "05/08/2018",
-    background: "white"
+    background: "white",
+    importance: "standard"
   },
   {
     title: "take care of the hamster",
     text: "1. Clean the aquarium. 2. Prepare seeds.",
     date: "03/08/2018",
-    background: "yellow"
+    background: "yellow",
+    importance: "standard"
   },
   {
     title: "Smart woman finish rich",
     text: 'Borrow "Smart woman finish rich" book from Magda',
     date: "10/08/2018",
-    background: "blue"
+    background: "blue",
+    importance: "standard"
   },
   {
     title: "Laundry",
     text: "Do the laundry (white and color)",
     date: "12/08/2018",
-    background: "blue"
+    background: "blue",
+    importance: "important"
   },
   {
     title: "Pizza with parents!",
     text: "Saturday! 18:00",
     date: "14/08/2018",
-    background: "orange"
+    background: "orange",
+    importance: "important"
   }
 ]
 
@@ -41,9 +46,11 @@ var app = new Vue ({
       newTitle: "",
       newText: "",
       date: "",
-      background: "white"
+      background: "white",
+      importance: "standard"
     },
     radioColor: "all",
+    noteImportance: "all",
     error: false
   },
   methods: {
@@ -55,12 +62,14 @@ var app = new Vue ({
             title: this.note.newTitle,
             text: this.note.newText,
             date: new Date().getDate()+"/"+(new Date().getMonth()+1)+"/"+new Date().getFullYear(),
-            background: this.note.background
+            background: this.note.background,
+            importance: this.note.importance
         })
         //clearing input fields
         this.note.newTitle = "";
         this.note.newText = "";
         this.note.background = "white";
+        this.note.importance = "standard";
         this.error = false;
       }
     }
