da8456a4a7e5fb0abc337ec923677f40a570a473
Katarzyna Wietecha-Mrzyglod <wietecha.katarzyna@gmail.com>
2018-08-14 09:00:26 +0200

    Created: sorting by color.

--- js/app.js
+++ js/app.js
@@ -10,6 +10,24 @@ var notes = [
     text: "1. Clean the aquarium. 2. Prepare seeds.",
     date: "03/08/2018",
     background: "yellow"
+  },
+  {
+    title: "Smart woman finish rich",
+    text: 'Borrow "Smart woman finish rich" book from Magda',
+    date: "10/08/2018",
+    background: "blue"
+  },
+  {
+    title: "Laundry",
+    text: "Do the laundry (white and color)",
+    date: "12/08/2018",
+    background: "blue"
+  },
+  {
+    title: "Pizza with parents!",
+    text: "Saturday! 18:00",
+    date: "14/08/2018",
+    background: "orange"
   }
 ]
 
@@ -24,7 +42,8 @@ var app = new Vue ({
       newText: "",
       date: "",
       background: "white"
-    }
+    },
+    radioColor: "all"
   },
   methods: {
     createNote: function(){
